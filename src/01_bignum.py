9108a0fada11b11266b1cdcb3041cdb1ab8b424d
andrewbastian <andrew.bastian@gmail.com>
2020-08-03 12:00:11 -0700

    Exponents

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,39 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+# There are 3 options:
+import math
+
+
+# option 1:
+def exponent_option_1(n):
+    return n ** 65536
+
+num = 2
+
+print('option 1:')
+
+print(exponent_option_1(num))
+
+# option 2:
+value = 2
+
+exponent_option_2 = pow(value, 65536)
+
+print('option 2:')
+
+print(exponent_option_2)
+
+
+# option 3:
+# needed to use a smaller exponent to get an answer
+# math.pow() will convert both arguments to floating point values
+# and will always return a float
+math_value = 2
+
+exponent_option_3 = math.pow(2, 6)
+
+print('option 3:')
+
+print(exponent_option_3)
